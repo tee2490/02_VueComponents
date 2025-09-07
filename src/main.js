7153ecfab52abb70da4b41023c3c479d2c308d76
Teeradet <tee2490@hotmail.com>
2025-09-07 11:11:46 +0700

    08 assignment local component

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import ContactUs from "./components/ContactUs.vue";
-import ButtonCounter from "./components/ButtonCounter.vue";
 
 const app = createApp(App);
-
-app.component("contact-us",ContactUs)
-app.component("button-counter", ButtonCounter);
+//global components
 
 app.mount("#app");
