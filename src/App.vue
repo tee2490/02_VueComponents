9f5afabc24c329e6b5269c6a9ad895f8009774fb
Teeradet <tee2490@hotmail.com>
2025-09-07 12:30:24 +0700

    09 list of contacts

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,46 @@
 <template>
   <div class="container">
     <div>{{ message }}</div>
-    <Contact name="Ben" :phone="123123123" :ownername="contactsOwner"></Contact>
-    <Contact
-      name="Jessica"
-      :phone="4343453"
-      :ownername="contactsOwner"
-      email="jessica@fakeemail.com"
-    ></Contact>
+    <div class="row">
+      <div class="col-12" v-for="contact in contacts" :key="contact.name">
+        <Contact
+          :name="contact.name"
+          :phone="contact.phone"
+          :ownername="contact.ownerName"
+          :email="contact.email"
+          :isFavorite="contact.isFavorite"
+        ></Contact>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref, reactive } from "vue";
 import Contact from "./components/Contact.vue";
 const message = "Hello Vue";
-const contactsOwner = "dotnetmastery";
+const ownerName = ref("dotnetmastery");
+const contacts = reactive([
+  {
+    name: "Bhrugen",
+    phone: 123131231,
+    ownerName: ownerName.value,
+    isFavorite: false,
+  },
+  {
+    name: "Bella",
+    phone: 5554443333,
+    ownerName: ownerName.value,
+    isFavorite: true,
+  },
+  {
+    name: "Ben",
+    phone: 33344455543,
+    ownerName: "",
+    email: "ben@dotnetmastery.com",
+    isFavorite: false,
+  },
+]);
 </script>
 
 <style></style>
--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="bg-info rounded p-4 m-2">
-    <h3>Name :{{ name }}</h3>
-    <p>Email : {{ email }}</p>
-    <p>Phone : {{ phone }}</p>
-    <p class="float-end small" v-if="ownername != ''">
-      *this contact info belongs to {{ ownername }}
-    </p>
+  <div class="bg-info rounded p-1 m-1">
+    <div class="row">
+      <div class="col-12">
+        <h3>Name :{{ name }}</h3>
+        <p>Email : {{ email }}</p>
+        <p>Phone : {{ phone }}</p>
+        <p class="float-end small" v-if="ownername != ''">
+          *this contact info belongs to {{ ownername }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
-
 <script setup>
 const props = defineProps({
   name: { type: String, required: true },
   phone: Number,
   ownername: String,
   email: { type: String, required: false, default: "-n/a-" },
+  isFavorite: Boolean,
 });
 </script>
