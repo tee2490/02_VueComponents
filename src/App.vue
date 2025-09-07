125915c3f217ae57e0fb0779a960276e96fc7e52
Teeradet <tee2490@hotmail.com>
2025-09-07 17:58:52 +0700

    11 slots

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,11 @@
 <template>
-  <div class="bg-black text pt-3" :style="{ height: '100vh' }">
-    <h1 class="text-center text-success">ContactOPedia</h1>
-    <div class="container">
-      <button
-        class="btn btn-primary text-black m-2"
-        @click="newVersion = !newVersion"
-      >
-        Toggle Componenet
-      </button>
-      <br />
-      <button
-        class="btn btn-primary text-black m-2"
-        @click="newVersion = false"
-      >
-        Lucky Number V1
-      </button>
-      <button class="btn btn-primary text-black m-2" @click="newVersion = true">
-        Lucky Number V2
-      </button>
-      <br />
-      <br />
-      <keep-alive :include="['LuckyNumber', 'LuckyNumberV2']">
-        <component :is="currentComponent" />
-      </keep-alive>
-    </div>
-  </div>
+  <LuckyNumberParentComponent>
+    <p class="pt-2">We have two version for picking lucky number</p>
+    <p>Click the button to toggle between the two versions</p>
+    <hr />
+  </LuckyNumberParentComponent>
 </template>
 
 <script setup>
-import { ref, reactive, provide, compile, computed } from "vue";
-import LuckyNumber from "./components/LuckyNumber.vue";
-import LuckyNumberV2 from "./components/LuckyNumberV2.vue";
-
-const newVersion = ref(true);
-
-const currentComponent = computed(() => {
-  return newVersion.value ? LuckyNumberV2 : LuckyNumber;
-});
+import LuckyNumberParentComponent from "./components/LuckyNumberParentComponent.vue";
 </script>
-
-<style></style>
