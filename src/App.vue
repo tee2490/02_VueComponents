385fe1cd636817c5f2c4c20f8450baafb92ee7fa
Teeradet <tee2490@hotmail.com>
2025-09-07 21:09:25 +0700

    11 Composible in multiple components

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
   <button @click="increment">Increment</button>
   <button @click="decrement">Decrement</button>
   <p>{{ count }}</p>
+
+  <br />
+  <ButtonCounter />
 </template>
 
 <script setup>
 import { useCounter } from "./composibles/useCounter";
-
+import ButtonCounter from "./components/ButtonCounter.vue";
 const { count, increment, decrement } = useCounter();
 </script>
--- src/components/ButtonCounter.vue
+++ src/components/ButtonCounter.vue
@@ -1,2 +1,11 @@
-<template>Counter</template>
-<script setup></script>
+<template>
+  <button @click="increment">++</button>
+  <span>{{ count }}</span>
+  <button @click="decrement">--</button>
+</template>
+
+<script setup>
+import { useCounter } from "../composibles/useCounter";
+
+const { count, increment, decrement } = useCounter();
+</script>
