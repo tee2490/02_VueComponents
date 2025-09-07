aa5721a1303f21d5971fa24a586e39ad8dd78520
Teeradet <tee2490@hotmail.com>
2025-09-07 17:42:34 +0700

    11 dynamic components

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,39 @@
   <div class="bg-black text pt-3" :style="{ height: '100vh' }">
     <h1 class="text-center text-success">ContactOPedia</h1>
     <div class="container">
-      <LuckyNumber></LuckyNumber>
+      <button
+        class="btn btn-primary text-black m-2"
+        @click="newVersion = !newVersion"
+      >
+        Toggle Componenet
+      </button>
       <br />
-      <LuckyNumberV2></LuckyNumberV2>
+      <button
+        class="btn btn-primary text-black m-2"
+        @click="newVersion = false"
+      >
+        Lucky Number V1
+      </button>
+      <button class="btn btn-primary text-black m-2" @click="newVersion = true">
+        Lucky Number V2
+      </button>
+      <br />
+      <br />
+      <component :is="currentComponent" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive, provide } from "vue";
+import { ref, reactive, provide, compile, computed } from "vue";
 import LuckyNumber from "./components/LuckyNumber.vue";
 import LuckyNumberV2 from "./components/LuckyNumberV2.vue";
+
+const newVersion = ref(true);
+
+const currentComponent = computed(() => {
+  return newVersion.value ? LuckyNumberV2 : LuckyNumber;
+});
 </script>
 
 <style></style>
