beb08ed550c92815c7deeca3a1f5b122a6233d61
Teeradet <tee2490@hotmail.com>
2025-09-07 13:44:14 +0700

    09 dynamically change class and button text

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="bg-info rounded p-1 m-1">
+  <div class="bg-info rounded p-1 pb-4 m-1">
     <div class="row">
-      <div class="col-12">
+      <div class="col-6">
         <h3>Name :{{ name }}</h3>
         <p>Email : {{ email }}</p>
         <p>Phone : {{ phone }}</p>
-        <p class="float-end small" v-if="ownername != ''">
-          *this contact info belongs to {{ ownername }}
-        </p>
+      </div>
+      <div class="col-3">
+        <button :class="[isFavorite ? 'btn btn-warning' : 'btn btn-success']">
+          {{ isFavorite ? "Remove from" : "Add to" }} Favorite
+        </button>
       </div>
     </div>
+    <span class="float-end small" v-if="ownername != ''">
+      *this contact info belongs to {{ ownername }}
+    </span>
   </div>
 </template>
 <script setup>
