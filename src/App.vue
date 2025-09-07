8a682a97cd112988e95c9b4551fe0e132ce07692
Teeradet <tee2490@hotmail.com>
2025-09-07 18:12:52 +0700

    11 who owns the slots data

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,18 @@
   <LuckyNumberParentComponent>
     <p class="pt-2">We have two version for picking lucky number</p>
     <p>Click the button to toggle between the two versions</p>
+    <button @click="showMessage">What will we learn?</button>
+    <h4 class="text-success">{{ message }}</h4>
     <hr />
   </LuckyNumberParentComponent>
 </template>
 
 <script setup>
 import LuckyNumberParentComponent from "./components/LuckyNumberParentComponent.vue";
+import { ref } from "vue";
+const message = ref("");
+
+function showMessage() {
+  message.value = "We will learn how to use slots";
+}
 </script>
